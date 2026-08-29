89dd2168a41cca6e0809c5f085a11fbd0905f4da
Yeisson Mora <yeissonrodriguez2014@gmail.com>
2026-08-29 17:04:28 -0500

    Se agrega la funciones de saldo y movimientos

--- menu.js
+++ menu.js
@@ -19,7 +19,7 @@ function registrase() {
     let password2 = prompt("Vuelva a ingresar la clave")
 
     if (password === password2) {
-        const newUser = { id: id, user: user, email: email, password: password, saldo: 0 };
+        const newUser = { id: id, user: user, email: email, password: password, saldo: 0, movimientos: [] };
         usuarios.push(newUser);
         localStorage.setItem("usuarios", JSON.stringify(usuarios));
         console.log("Te has registrado exitosamente !");
@@ -53,6 +53,7 @@ function inicio() {
                 console.log("Inicio de sesion exitoso");
                 acceso = true;
                 usuarioActivo = usuarioEncontrado
+                menuTransacciones();
                 break;
 
 
@@ -75,24 +76,32 @@ function inicio() {
 
     }
 }
+function menuTransacciones() {
+    let continuar = true;
 
-function menuTransacciones(){
-let consultaMovimientos = Number(prompt("1. Retirar\n 2.Consultar Saldo\n  3. Consignar\n 4. Consultar Movimientos\n 5. Salir\n "))
-switch (consultaMovimientos) {
-    case 1: retirar(); break;
-    case 2: saldo(); break;
-    case 3: consignar(); break;
-    case 4: movimientos(); break;
-    case 5: salir(); break;
-}
-}
+    while (continuar) {
+        let consultaMovimientos = Number(prompt("1. Retirar\n 2.Consultar Saldo\n  3. Consignar\n 4. Consultar Movimientos\n 5. Salir\n "));
 
+        switch (consultaMovimientos) {
+            case 1: retirar(); break;
+            case 2: saldo(); break;
+            case 3: consignar(); break;
+            case 4: movimientos(); break;
+            case 5:
+                console.log("Sesión finalizada. ¡Gracias por usar Mi Plata!");
+                continuar = false;
+                break;
+        }
+    }
+}
 function retirar() {
     if (usuarioActivo.saldo > 0) {
         let solicitudRetirar = Number(prompt("¿Cuánto desea retirar?"));
 
         if (solicitudRetirar <= usuarioActivo.saldo) {
             usuarioActivo.saldo = usuarioActivo.saldo - solicitudRetirar;
+            usuarioActivo.movimientos.push({ fecha: new Date().toLocaleString(), tipo: "Retiro",
+             valor: solicitudRetirar, saldo: usuarioActivo.saldo });
             localStorage.setItem("usuarios", JSON.stringify(usuarios));
             console.log(`Has retirado ${solicitudRetirar}. Tu nuevo saldo es ${usuarioActivo.saldo}`);
         } else {
@@ -103,10 +112,39 @@ function retirar() {
     }
 }
 
-function consignar(){
+function consignar() {
     let solicitudConsignar = Number(prompt(" Ingrese valor a consignar "))
-    usuarioActivo.saldo = usuarioActivo.saldo + solicitudConsignar
-    localStorage.setItem("usuarios", JSON.stringify(usuarios));
-    console.log(`Has consignado exitosamente ${solicitudConsignar}. Tu nuevo saldo es ${usuarioActivo.saldo}`);
+    if (solicitudConsignar > 0) {
+        usuarioActivo.saldo = usuarioActivo.saldo + solicitudConsignar
+        usuarioActivo.movimientos.push({ fecha: new Date().toLocaleString(), tipo: "Consignación", 
+        valor: solicitudConsignar, saldo: usuarioActivo.saldo });
+        localStorage.setItem("usuarios", JSON.stringify(usuarios));
+        console.log(`Has consignado exitosamente ${solicitudConsignar}. Tu nuevo saldo es ${usuarioActivo.saldo}`);
 
+    } else {
+        console.log("Valor no valido");
+    }
+}
+
+function saldo() {
+    console.log(`Tu saldo actual es ${usuarioActivo.saldo}`);
+}
+
+function movimientos(){
+     if (usuarioActivo.movimientos.length === 0) {
+    console.log(" Aún no tiene movimientos ");
+    return;
+}
+     console.log(` *** HISTORIAL DE MOVIMIENTOS ***`);
+    for(let i=0; i < usuarioActivo.movimientos.length; i ++){
+        let registro = usuarioActivo.movimientos[i];
+        console.log(`
+         FECHA Y HORA --> ${registro.fecha}
+         CONCEPTO ------> ${registro.tipo}  
+         VALOR ---------> ${registro.valor}
+         SALDO ---------> ${registro.saldo}`);
+
+        
+
+    }
 }
