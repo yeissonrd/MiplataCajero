772f2921c2b48c5d536759a759f356b86e3edac8
Yeisson Mora <yeissonrodriguez2014@gmail.com>
2026-09-04 18:09:27 -0500

    Se suben las ultimas funciones de movimientos y saldo, tambien se hace algunas correciones.

--- menu.js
+++ menu.js
@@ -14,9 +14,16 @@ switch (respuesta) {
 function registrase() {
     let id = prompt(" Ingrese idetificación ")
     let user = prompt("Ingrese usuario")
+    let usuarioExistente = usuarios.find(usuario => usuario.user === user);
+    if (usuarioExistente) {
+        console.log("Ese usuario ya existe, elige otro");
+        return;
+    }
     let email = prompt("Ingrese correo ")
     let password = prompt("Ingrese clave")
     let password2 = prompt("Vuelva a ingresar la clave")
+
+    
 
     if (password === password2) {
         const newUser = { id: id, user: user, email: email, password: password, saldo: 0, movimientos: [] };
@@ -91,6 +98,7 @@ function menuTransacciones() {
                 console.log("Sesión finalizada. ¡Gracias por usar Mi Plata!");
                 continuar = false;
                 break;
+            default: console.log("Ingrese un valor valido");
         }
     }
 }
@@ -98,10 +106,13 @@ function retirar() {
     if (usuarioActivo.saldo > 0) {
         let solicitudRetirar = Number(prompt("¿Cuánto desea retirar?"));
 
+
         if (solicitudRetirar <= usuarioActivo.saldo) {
             usuarioActivo.saldo = usuarioActivo.saldo - solicitudRetirar;
-            usuarioActivo.movimientos.push({ fecha: new Date().toLocaleString(), tipo: "Retiro",
-             valor: solicitudRetirar, saldo: usuarioActivo.saldo });
+            usuarioActivo.movimientos.push({
+                fecha: new Date().toLocaleString(), tipo: "Retiro",
+                valor: solicitudRetirar, saldo: usuarioActivo.saldo
+            });
             localStorage.setItem("usuarios", JSON.stringify(usuarios));
             console.log(`Has retirado ${solicitudRetirar}. Tu nuevo saldo es ${usuarioActivo.saldo}`);
         } else {
@@ -116,8 +127,10 @@ function consignar() {
     let solicitudConsignar = Number(prompt(" Ingrese valor a consignar "))
     if (solicitudConsignar > 0) {
         usuarioActivo.saldo = usuarioActivo.saldo + solicitudConsignar
-        usuarioActivo.movimientos.push({ fecha: new Date().toLocaleString(), tipo: "Consignación", 
-        valor: solicitudConsignar, saldo: usuarioActivo.saldo });
+        usuarioActivo.movimientos.push({
+            fecha: new Date().toLocaleString(), tipo: "Consignación",
+            valor: solicitudConsignar, saldo: usuarioActivo.saldo
+        });
         localStorage.setItem("usuarios", JSON.stringify(usuarios));
         console.log(`Has consignado exitosamente ${solicitudConsignar}. Tu nuevo saldo es ${usuarioActivo.saldo}`);
 
@@ -130,13 +143,13 @@ function saldo() {
     console.log(`Tu saldo actual es ${usuarioActivo.saldo}`);
 }
 
-function movimientos(){
-     if (usuarioActivo.movimientos.length === 0) {
-    console.log(" Aún no tiene movimientos ");
-    return;
-}
-     console.log(` *** HISTORIAL DE MOVIMIENTOS ***`);
-    for(let i=0; i < usuarioActivo.movimientos.length; i ++){
+function movimientos() {
+    if (usuarioActivo.movimientos.length === 0) {
+        console.log(" Aún no tiene movimientos ");
+        return;
+    }
+    console.log(` *** HISTORIAL DE MOVIMIENTOS ***`);
+    for (let i = 0; i < usuarioActivo.movimientos.length; i++) {
         let registro = usuarioActivo.movimientos[i];
         console.log(`
          FECHA Y HORA --> ${registro.fecha}
@@ -144,7 +157,7 @@ function movimientos(){
          VALOR ---------> ${registro.valor}
          SALDO ---------> ${registro.saldo}`);
 
-        
+
 
     }
 }
